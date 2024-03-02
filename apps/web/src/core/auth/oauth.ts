9db5a3835bf4ecd71185b6fc70a2aef26de9161c
Gus <gus.cuddy@gmail.com>
2024-03-01 22:35:01 -0500

    delete old files

--- apps/web/src/core/auth/oauth.ts
+++
@@ -1,3 +0,0 @@
-import { Discord } from "arctic";
-
-export const discord = new Discord(process.env.DISCORD_ID as string, process.env.DISCORD_SECRET as string, "http://localhost:3000/login/discord/callback");
