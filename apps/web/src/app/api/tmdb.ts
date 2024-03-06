ff567fa64c057a1827469eb4057d173c38443010
Gus <gus.cuddy@gmail.com>
2024-03-06 16:40:10 -0500

    fix: write our own tmdb wrapper that uses normal fetch

--- apps/web/src/app/api/tmdb.ts
+++ apps/web/src/app/api/tmdb.ts
@@ -1,5 +1,4 @@
 import { uncached_getMovie } from "@/utils/tmdb";
 import { cache } from "react";
-import "server-only";
 
 export const getMovie = cache(uncached_getMovie);
--- apps/web/src/lib/tmdb.ts
+++ apps/web/src/lib/tmdb.ts
@@ -1,4 +1,71 @@
-import { TMDB } from "tmdb-ts";
+import type { AppendToResponse, AppendToResponseMovieKey, AppendToResponsePersonKey, MovieDetails, Movie, Person, Search, PersonDetails } from "tmdb-ts";
 import { env } from "@/env.mjs";
+
+
+export class TMDB {
+  private readonly accessToken: string;
+
+  private request = async <T>(path: `/${string}`, params?: Record<string, string | number>): Promise<T> => {
+    const url = new URL(`https://api.themoviedb.org/3${path}`);
+
+    if (params) {
+      for (const [key, value] of Object.entries(params)) {
+        url.searchParams.set(key, String(value));
+      }
+    }
+
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${this.accessToken}`,
+        'Content-Type': 'application/json;charset=utf-8',
+      },
+    })
+
+    if (!response.ok) {
+      return Promise.reject((await response.json()) as {
+        status_code: number;
+        status_message: string;
+        success: boolean;
+      });
+    }
+
+    return await response.json<T>()
+  }
+
+  constructor(accessToken: string) {
+    this.accessToken = accessToken;
+  }
+
+
+  // TODO: define search, movie/details, people
+
+  search = {
+    movies: (params: {
+      query: string;
+      year?: number;
+    }) => {
+      return this.request<Search<Movie>>("/search/movie", params);
+    },
+
+    people: (params: {
+      query: string;
+    }) => {
+      return this.request<Search<Person>>("/search/person", params);
+    },
+  }
+
+  movies = {
+    details: <T extends AppendToResponseMovieKey[]>(id: number, append_to_response: T) => {
+      return this.request<AppendToResponse<MovieDetails, T, 'movie'>>(`/movie/${id}`, { append_to_response: append_to_response.join(",") });
+    }
+  }
+
+  people = {
+    details: <T extends AppendToResponsePersonKey[]>(id: number, append_to_response: T) => {
+      return this.request<AppendToResponse<PersonDetails, T, 'person'>>(`/person/${id}`, { append_to_response: append_to_response.join(",") });
+    }
+  }
+}
 
 export const tmdb = new TMDB(env.TMDB_TOKEN);
