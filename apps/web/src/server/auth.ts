5a1bc7dcd084b1551d18adc9001708dd78c045be
Gus <gus.cuddy@gmail.com>
2024-03-06 20:52:46 -0500

    error handling

--- apps/web/src/server/auth.ts
+++ apps/web/src/server/auth.ts
@@ -4,23 +4,24 @@ import { cache } from "react";
 
 export const getPageSession = cache(async () => {
     const auth_session = cookies().get("auth_session");
-    // lol, there's gotta be a better way...
-    const authData = await client.auth.validate.$get({}, {
-        headers: {
-            Cookie: auth_session ? `auth_session=${auth_session.value}` : "",
+    try {
+        const authData = await client.auth.validate.$get({}, {
+            // lol, there's gotta be a better way...
+            headers: {
+                Cookie: auth_session ? `auth_session=${auth_session.value}` : "",
+            }
+        }).then(res => res.json())
+        if (authData) {
+            const { session } = authData
+            try {
+                if (session) cookies().set("auth_session", session.id)
+                else cookies().delete("auth_session")
+            } catch (e) {
+                console.error(e);
+            }
         }
-    }).then(res => res.json())
-        .catch(e => {
-            console.error(e);
-        })
-    if (authData) {
-        const { session } = authData
-        try {
-            if (session) cookies().set("auth_session", session.id)
-            else cookies().delete("auth_session")
-        } catch (e) {
-            console.error(e);
-        }
+        return authData
+    } catch {
+        return null
     }
-    return authData
 })
