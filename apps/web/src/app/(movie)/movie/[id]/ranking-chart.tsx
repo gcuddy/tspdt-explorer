07c1612c110bbfebdb6a120d016f8176c378aa5e
Gus <gus@guscuddy.com>
2024-11-02 13:19:24 -0400

    attempts at updating

--- apps/web/src/app/(movie)/movie/[id]/ranking-chart.tsx
+++ apps/web/src/app/(movie)/movie/[id]/ranking-chart.tsx
@@ -16,6 +16,8 @@ const COLORS = [
   "hsla(300, 50%, 72%, 0.75)",
 ];
 
+
+
 export function RankingChart({
   data,
   enablePoints = true,
@@ -27,6 +29,8 @@ export function RankingChart({
 }) {
 
   const router = useRouter();
+
+  console.log({ data, enablePoints, colors })
 
   const { ticks, minYear, maxYear } = useMemo(() => {
     const years = new Set<number>();
@@ -66,6 +70,109 @@ export function RankingChart({
   }, [data]);
 
   console.log({ actualData });
+
+
+  return (
+    <ResponsiveLine
+      data={actualData}
+      margin={{ top: 50, right: 160, bottom: 50, left: 60 }}
+      xScale={{ type: 'linear' }}
+      yScale={{ type: 'linear', stacked: true, min: 0, max: 2500 }}
+      yFormat=" >-.2f"
+      curve="monotoneX"
+      axisTop={null}
+      axisRight={{
+        tickValues: [
+          0,
+          500,
+          1000,
+          1500,
+          2000,
+          2500
+        ],
+        tickSize: 5,
+        tickPadding: 5,
+        tickRotation: 0,
+        format: '.2s',
+        legend: '',
+        legendOffset: 0
+      }}
+      axisBottom={{
+        tickValues: [
+          0,
+          20,
+          40,
+          60,
+          80,
+          100,
+          120
+        ],
+        tickSize: 5,
+        tickPadding: 5,
+        tickRotation: 0,
+        format: '.2f',
+        legend: 'price',
+        legendOffset: 36,
+        legendPosition: 'middle'
+      }}
+      axisLeft={{
+        tickValues: [
+          0,
+          500,
+          1000,
+          1500,
+          2000,
+          2500
+        ],
+        tickSize: 5,
+        tickPadding: 5,
+        tickRotation: 0,
+        format: '.2s',
+        legend: 'volume',
+        legendOffset: -40,
+        legendPosition: 'middle'
+      }}
+      enableGridX={false}
+      colors={{ scheme: 'spectral' }}
+      lineWidth={1}
+      pointSize={4}
+      pointColor={{ theme: 'background' }}
+      pointBorderWidth={1}
+      pointBorderColor={{ from: 'serieColor' }}
+      pointLabel="data.yFormatted"
+      pointLabelYOffset={-12}
+      enableTouchCrosshair={true}
+      useMesh={true}
+      gridXValues={[0, 20, 40, 60, 80, 100, 120]}
+      gridYValues={[0, 500, 1000, 1500, 2000, 2500]}
+      legends={[
+        {
+          anchor: 'bottom-right',
+          direction: 'column',
+          justify: false,
+          translateX: 140,
+          translateY: 0,
+          itemsSpacing: 2,
+          itemDirection: 'left-to-right',
+          itemWidth: 80,
+          itemHeight: 12,
+          itemOpacity: 0.75,
+          symbolSize: 12,
+          symbolShape: 'circle',
+          symbolBorderColor: 'rgba(0, 0, 0, .5)',
+          effects: [
+            {
+              on: 'hover',
+              style: {
+                itemBackground: 'rgba(0, 0, 0, .03)',
+                itemOpacity: 1
+              }
+            }
+          ]
+        }
+      ]}
+    />
+  )
 
   return (
     <ResponsiveLine
@@ -267,7 +374,7 @@ export function RankingChart({
       }
       legends={
         data.length === 1
-          ? undefined
+          ? []
           : [
             // TODO: this is not great, probably shuold implement own
             //   {
