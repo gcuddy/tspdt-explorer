bee6f62788c6b99f80ad767c662c52ce9d0c5d2b
Gus <gus@guscuddy.com>
2024-11-02 11:56:57 -0400

    fix errors

--- apps/web/src/components/people-list.tsx
+++ apps/web/src/components/people-list.tsx
@@ -1,58 +1,58 @@
-import React from "react";
+import * as React from "react";
 import Link from "next/link";
 
 export type Person = {
-    id: string;
-    name: string;
+  id: string;
+  name: string;
 };
 
 
 export function PersonLink({
-    person,
+  person,
 }: {
-    person: Person;
+  person: Person;
 }) {
-    return (
-        <Link href={`/director/${person.id}`} className="text-zinc-300">
-            {person.name}
-        </Link>
-    );
+  return (
+    <Link href={`/director/${person.id}`} className="text-zinc-300">
+      {person.name}
+    </Link>
+  );
 }
 
 export function PeopleList({
-    people,
-    prefix,
+  people,
+  prefix,
 }: {
-    people: Array<Person>;
-    prefix?: string;
+  people: Array<Person>;
+  prefix?: string;
 }) {
-    if (people.length === 1) {
-        return (
-            <span className="truncate">
-                {prefix} <PersonLink person={people[0]} />
-            </span>
-        );
-    }
-
+  if (people.length === 1) {
     return (
-        <span className="truncate">
-            {prefix}{" "}
-            {people.map((director, index) => {
-                if (index === people.length - 1) {
-                    return (
-                        <span key={director.id}>
-                            and <PersonLink person={director} />
-                        </span>
-                    );
-                }
-
-                return (
-                    <>
-                        <PersonLink key={director.id} person={director} />
-                        {", "}
-                    </>
-                );
-            })}
-        </span>
+      <span className="truncate">
+        {prefix} <PersonLink person={people[0]} />
+      </span>
     );
+  }
+
+  return (
+    <span className="truncate">
+      {prefix}{" "}
+      {people.map((director, index) => {
+        if (index === people.length - 1) {
+          return (
+            <span key={director.id}>
+              and <PersonLink person={director} />
+            </span>
+          );
+        }
+
+        return (
+          <React.Fragment key={director.id}>
+            <PersonLink key={director.id} person={director} />
+            {", "}
+          </React.Fragment>
+        );
+      })}
+    </span>
+  );
 }
